d99364de26be4f4efb8cd77dcb7f4da4550217ed
Anish-Agnihotri <contact@anishagnihotri.com>
2019-01-06 15:35:37 -0500

    Updates

---
+++ precache-manifest.1e53244a79010c28a5835be043108ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dappnode/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8d532f825bfea3f68720",
+    "url": "/dappnode/static/js/runtime~main.8d532f82.js"
+  },
+  {
+    "revision": "49868229f4f1682220e6",
+    "url": "/dappnode/static/js/main.49868229.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/dappnode/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "49868229f4f1682220e6",
+    "url": "/dappnode/static/css/main.c4ebc18c.chunk.css"
+  },
+  {
+    "revision": "ce6294a4b43e806aa5cc6ac03b92d113",
+    "url": "/dappnode/index.html"
+  }
+];
